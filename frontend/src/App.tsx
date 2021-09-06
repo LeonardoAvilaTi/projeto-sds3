e332d3199e150f28f5ead75f5b5b7bf7cf4e0fa2
Leonardo Avila <lavila.mac.ti@gmail.com>
2021-09-06 16:18:16 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from 'components/DataTable';
+import NavBar from 'components/NavBar';
+import Footer from 'components/NavBar/Footer';
 import React from 'react';
 
 function App() {
   return (
-    <div>
-      <h1 className = "text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className ="container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 

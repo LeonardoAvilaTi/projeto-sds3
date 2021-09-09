f4dc4200e091f76fc09622921d7a08f9d0ad9164
Leonardo Avila <lavila.mac.ti@gmail.com>
2021-09-08 22:19:17 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SaleRepository.java
@@ -2,10 +2,10 @@ package com.devsuperior.dsvendas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.entities.Sale;
 
 
-public interface SellerRepository extends JpaRepository<Seller, Long>{
+public interface SaleRepository extends JpaRepository<Sale, Long>{
 	
 	
 

41b8b7e37dfd6dfb2aff4cb4ed69775bcd02c9df
Leonardo Avila <lavila.mac.ti@gmail.com>
2021-09-10 23:21:25 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -10,40 +11,45 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //forma errada
-    let chartData : ChartData = {labels : [] , series : []};
-    
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then(response =>{
-            const data = response.data as SaleSum[];
-            const mylabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
+    /*forma errada
+    let chartData : ChartData = {labels : [] , series : []};*/
 
-            chartData =  {labels : mylabels  , series : mySeries};   
+    /*Hook: useState Manter estado no componente
+    Hook: useEffect Executar algo na instanciação ou destruição do componente, observar estado*/
 
-            console.log ( chartData) ;
-        })
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
+    useEffect(() => {
 
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const mylabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
 
+                setChartData({ labels: mylabels, series: mySeries });
+
+                /*console.log(chartData);*/
+            })
+
+    }, []);
 
     /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }*/
-
     
     const options = {
         legend: {
             show: true
         }
-    }    
-    
+    }
+
     return (
-        <Chart options={{ ...options,labels:chartData.labels}} 
-               series={chartData.series}
-               type="donut"
-               height="240"  />
-            );
+        <Chart options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240" />
+    );
 
 
     /*    return (
